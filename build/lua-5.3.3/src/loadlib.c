e592869c1e61bb79bb2c9e80fd9467b080408c8b
h34tn <heath.luo@gmail.com>
2018-10-19 19:50:09 +0800

    require add cache parameter

--- build/lua-5.3.3/src/loadlib.c
+++ build/lua-5.3.3/src/loadlib.c
@@ -557,7 +557,14 @@ static void findloader (lua_State *L, const char *name) {
 
 static int ll_require (lua_State *L) {
   const char *name = luaL_checkstring(L, 1);
-  lua_settop(L, 1);  /* _LOADED table will be at index 2 */
+  int cache = TRUE;
+  int top = 1;
+  if(lua_isboolean(L, -2))
+  {
+    top = 2;
+    cache = lua_toboolean(L, -2);
+  }  
+  lua_settop(L, top);  /* _LOADED table will be at index 3 */
   lua_getfield(L, LUA_REGISTRYINDEX, "_LOADED");
   lua_getfield(L, 2, name);  /* _LOADED[name] */
   if (lua_toboolean(L, -1))  /* is it there? */
@@ -568,12 +575,15 @@ static int ll_require (lua_State *L) {
   lua_pushstring(L, name);  /* pass name as argument to module loader */
   lua_insert(L, -2);  /* name is 1st argument (before search data) */
   lua_call(L, 2, 1);  /* run loader to load module */
-  if (!lua_isnil(L, -1))  /* non-nil return? */
-    lua_setfield(L, 2, name);  /* _LOADED[name] = returned value */
-  if (lua_getfield(L, 2, name) == LUA_TNIL) {   /* module set no value? */
-    lua_pushboolean(L, 1);  /* use true as result */
-    lua_pushvalue(L, -1);  /* extra copy to be returned */
-    lua_setfield(L, 2, name);  /* _LOADED[name] = true */
+  if(cache)
+  {
+    if (!lua_isnil(L, -1))  /* non-nil return? */
+      lua_setfield(L, 2, name);  /* _LOADED[name] = returned value */
+    if (lua_getfield(L, 2, name) == LUA_TNIL) {   /* module set no value? */
+      lua_pushboolean(L, 1);  /* use true as result */
+      lua_pushvalue(L, -1);  /* extra copy to be returned */
+      lua_setfield(L, 2, name);  /* _LOADED[name] = true */
+    }
   }
   return 1;
 }
--- build/lua-5.3.4/src/loadlib.c
+++ build/lua-5.3.4/src/loadlib.c
@@ -600,7 +600,14 @@ static void findloader (lua_State *L, const char *name) {
 
 static int ll_require (lua_State *L) {
   const char *name = luaL_checkstring(L, 1);
-  lua_settop(L, 1);  /* LOADED table will be at index 2 */
+ int cache = TRUE;
+  int top = 1;
+  if(lua_isboolean(L, -2))
+  {
+    top = 2;
+    cache = lua_toboolean(L, -2);
+  }  
+  lua_settop(L, top);  /* LOADED table will be at index 3 */
   lua_getfield(L, LUA_REGISTRYINDEX, LUA_LOADED_TABLE);
   lua_getfield(L, 2, name);  /* LOADED[name] */
   if (lua_toboolean(L, -1))  /* is it there? */
@@ -611,12 +618,15 @@ static int ll_require (lua_State *L) {
   lua_pushstring(L, name);  /* pass name as argument to module loader */
   lua_insert(L, -2);  /* name is 1st argument (before search data) */
   lua_call(L, 2, 1);  /* run loader to load module */
-  if (!lua_isnil(L, -1))  /* non-nil return? */
-    lua_setfield(L, 2, name);  /* LOADED[name] = returned value */
-  if (lua_getfield(L, 2, name) == LUA_TNIL) {   /* module set no value? */
-    lua_pushboolean(L, 1);  /* use true as result */
-    lua_pushvalue(L, -1);  /* extra copy to be returned */
-    lua_setfield(L, 2, name);  /* LOADED[name] = true */
+  if(cache)
+  {
+    if (!lua_isnil(L, -1))  /* non-nil return? */
+      lua_setfield(L, 2, name);  /* _LOADED[name] = returned value */
+    if (lua_getfield(L, 2, name) == LUA_TNIL) {   /* module set no value? */
+      lua_pushboolean(L, 1);  /* use true as result */
+      lua_pushvalue(L, -1);  /* extra copy to be returned */
+      lua_setfield(L, 2, name);  /* _LOADED[name] = true */
+    }
   }
   return 1;
 }
